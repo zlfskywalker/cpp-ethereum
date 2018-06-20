cc260b9327f3ff2de689963a0650db5e7f296737
Dimitry <dimitry@ethereum.org>
2018-06-20 14:57:47 +0300

    fix vmtrace

--- libethereum/State.cpp
+++ libethereum/State.cpp
@@ -612,12 +612,12 @@ std::pair<ExecutionResult, TransactionReceipt> State::execute(EnvInfo const& _en
     auto onOp = _onOp;
 #if ETH_VMTRACE
 	// Run the existing onOp function and the tracer
-	onOp = [&_onOp, &e](uint64_t _steps, uint64_t PC, Instruction inst, bigint
-			newMemSize, bigint gasCost, bigint gas, VMFace const* _vm,
-			ExtVMFace const* voidExt) {
-		_onOp(_steps, PC, inst, newMemSize, gasCost, gas, _vm, voidExt);
-		e.simpleTrace();
-	};
+    onOp = [&_onOp, &e](uint64_t _steps, uint64_t PC, Instruction inst, bigint newMemSize,
+               bigint gasCost, bigint gas, VMFace const* _vm, ExtVMFace const* voidExt) {
+        if (_onOp)
+            _onOp(_steps, PC, inst, newMemSize, gasCost, gas, _vm, voidExt);
+        e.simpleTrace();
+    };
 #endif
 
     u256 const startGasUsed = _envInfo.gasUsed();
